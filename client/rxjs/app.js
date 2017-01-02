da5fab1936153a3ada9ec4a43f6ca17f3615b0a0
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2017-01-02 12:13:26 +0000

    Got RxJS version working

--- client/rxjs/app.js
+++ client/rxjs/app.js
@@ -1,4 +1,16 @@
-//import Rx from 'rxjs/Rx';
 import $ from 'jquery';
+const Rx = require('@reactivex/rxjs');
 
-alert('Hello');
+const INCREMENT = 'INCREMENT';
+const DECREMENT = 'DECREMENT';
+
+$(document).ready(() => {
+    const $counter = $('#counter');
+    const upClicks = Rx.Observable.fromEvent($('#incrementBtn'), 'click');
+    const downClicks = Rx.Observable.fromEvent($('#decrementBtn'), 'click');
+    const ups = upClicks.mapTo(INCREMENT);
+    const downs = downClicks.mapTo(DECREMENT);
+    const upsAndDowns = Rx.Observable.merge(ups, downs);
+    const values = upsAndDowns.scan((acc, curr) => acc + (curr === INCREMENT ? 1 : -1), 0).startWith(0);
+    const subscription = values.subscribe(v => $counter.html(v));
+});
