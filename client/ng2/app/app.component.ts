54b7363a265cfd1494fa2a101627183e088c1259
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2017-01-04 23:47:20 +0000

    Got simplistic ng2 app working

--- client/ng2/app/app.component.ts
+++ client/ng2/app/app.component.ts
@@ -1,13 +1,32 @@
 import {Component} from '@angular/core';
+import * as actions from './actions';
+import counter from './reducers';
+import { createStore, Store } from 'redux';
 
 @Component({
     selector: 'app',
     template: `
         <div>
-            <button>Up</button>
-            <button>Down</button>
-            <span></span>
+            <button (click)="onIncrement()">Up</button>
+            <button (click)="onDecrement()">Down</button>
+            <span>{{ counter }}</span>
         </div>`
 })
 export class AppComponent {
+    private store: Store<number>;
+    private counter: number;
+    constructor() {
+        this.store = createStore<number>(counter);
+        this.store.subscribe(() => this.readState());
+        this.readState();
+    }
+    onIncrement() {
+        this.store.dispatch(actions.increment())
+    }
+    onDecrement() {
+        this.store.dispatch(actions.decrement())
+    }
+    private readState() {
+        this.counter = this.store.getState();
+    }
 }
